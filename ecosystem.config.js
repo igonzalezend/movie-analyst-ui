895334e40db7d5bd5c9f134b721c7d72ac2c4492
Ivan Gonzalez <Ivan.Gonzalez@endava.com>
2019-02-13 16:12:52 -0500

    ecosystem modified

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps : [{
-      name: "app",
-      script: "./app.js",
+      name: "Movies_UI",
+      script: "./server.js",
       env: {
         BACKEND_URL: "10.0.6.189:3000",
         PORT: "3030", 
